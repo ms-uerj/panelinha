9b22b08b1b3f2de751d3c9092b0c89d4d0e00494
feliperedhead <feliperedhead@e4e8c67f-8f4f-f301-b73d-0bd64106ad73>
2010-09-19 15:41:54 +0000

    Modelo: Onde ficam as classes que representam as entidades do projeto: usuário, item, troca entre outras

---
+++ trunk/TrocaOuNaoTroca/src/model/Usuario.java
@@ -0,0 +1,77 @@
+package model;
+
+public class Usuario {
+
+	private String matricula;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	private String sexo;
+
+	
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(String email, String matricula, String nome, String senha,
+			String sexo, String sobrenome) {
+		super();
+		this.email = email;
+		this.matricula = matricula;
+		this.nome = nome;
+		this.senha = senha;
+		this.sexo = sexo;
+		this.sobrenome = sobrenome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+}
